3ad447f96aab43b3dc8e83c39a5b96eb7af4f657
martinfzz <forget.l.martin@gmail.com>
2021-04-07 15:17:37 +0200

    Add random pick players

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const Carl = new Assassin('Carl');
 const Harry = new Wizard('Harry');
 const Jack = new Pirate('Jack');
 
+chooseRandPlayers = () => {
+  let players = [Grace, Ulder, Moana, Draven, Carl, Harry, Jack];
+  for(let i=0; i<5; i++){
+    randPlayers = Math.floor(Math.random() * players.length);
+    game.addPlayer(players[randPlayers]);
+    players.splice(randPlayers, 1);
+  }
+  console.log(players)
+}
+
 const game = new Game();
-game.addPlayer(Grace);
-game.addPlayer(Ulder);
-game.addPlayer(Moana);
-game.addPlayer(Draven);
-game.addPlayer(Carl);
-game.addPlayer(Harry);
-game.addPlayer(Jack);
+chooseRandPlayers();
 
 const turn = new Turn();
 game.startGame();
